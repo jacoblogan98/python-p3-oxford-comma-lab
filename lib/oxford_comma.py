ac7dbc4f0ac38eded7c816798756a14deb392362
Jacob Logan <91486310+jacoblogan98@users.noreply.github.com>
2023-02-19 16:38:01 -0500

    passed tests

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,6 @@
 def oxford_comma(items):
-    return None
+    if len(items) == 1: return items[0]
+    if len(items) == 2: return " and ".join(items)
+
+    last_item = items.pop()
+    return ", ".join(items) + f", and {last_item}"
